9d168589cb512b44b7fa7c7bbd6b64e687a3975a
Patryk Miłek <patrykmilek96@gmail.com>
2018-11-18 18:29:08 +0100

    Fix unit test on windows

--- csharp/ApprovalTest.cs
+++ csharp/ApprovalTest.cs
@@ -11,17 +11,23 @@ namespace csharp
         [Test]
         public void ThirtyDays()
         {
-            var lines = File.ReadAllLines("ThirtyDays.txt");
+            var filePath = $"{AppDomain.CurrentDomain.BaseDirectory}\\ThirtyDays.txt";
+            var lines = File.ReadAllLines(filePath);
 
-            StringBuilder fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
+            var fakeOutput = new StringBuilder();
+            var stringWriter = new StringWriter(fakeOutput)
+            {
+                NewLine = "\n"
+            };
+
+            Console.SetOut(stringWriter);
             Console.SetIn(new StringReader("a\n"));
 
             Program.Main(new string[] { });
-            String output = fakeoutput.ToString();
+            var output = fakeOutput.ToString();
 
             var outputLines = output.Split('\n');
-            for(var i = 0; i<Math.Min(lines.Length, outputLines.Length); i++) 
+            for (var i = 0; i < Math.Min(lines.Length, outputLines.Length); i++)
             {
                 Assert.AreEqual(lines[i], outputLines[i]);
             }
