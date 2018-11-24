708934a5af47a399914a35cf6de0e788b5659667
Patryk Miłek <patrykmilek96@gmail.com>
2018-11-24 20:52:32 +0100

    Minor code refactoring

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,7 +9,7 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
+            var Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
                 new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
@@ -44,9 +44,9 @@ namespace csharp
             {
                 Console.WriteLine("-------- day " + i + " --------");
                 Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
+                foreach (var item in Items)
                 {
-                    System.Console.WriteLine(Items[j]);
+                    Console.WriteLine(item);
                 }
                 Console.WriteLine("");
                 app.UpdateQuality();
