277cf5c71b32ae398fa584bcae6475d36adab40f
Patryk Miłek <pmilek@INF.UG.EDU.PL>
2018-11-25 14:37:34 +0100

    Rewrite logic as LINQ statements

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace csharp
 {
@@ -12,85 +13,60 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            foreach (var item in Items)
-            {
-                if (!IsItemAgedBrie(item) && !IsItemBackstagePass(item))
-                {
-                    if (item.Quality > 0)
-                    {
-                        if (!IsItemSulfuras(item))
-                        {
-                            item.Quality = item.Quality - 1;
-                            if(item.Name == "Conjured Mana Cake")
-                            {
-                                if (item.Quality > 0)
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                    }
-                }
-                else
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
+            var regularItems = Items.Where(x => !IsItemAgedBrie(x) && !IsItemBackstagePass(x) && !IsItemSulfuras(x))
+                .Where(x => x.Quality > 0)
+                .ToList();
 
-                        if (IsItemBackstagePass(item))
-                        {
-                            if (item.SellIn < 11)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
+            regularItems.ForEach(DecreaseItemQuality);
+            /*regularItems.Where(x => x.SellIn < 0)
+                .ToList()
+                .ForEach(DecreaseItemQuality);*/
 
-                            if (item.SellIn < 6)
-                            {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
+            regularItems.Where(x => IsItemConjured(x))
+                .ToList()
+                .ForEach(DecreaseItemQuality);
 
-                if (!IsItemSulfuras(item))
-                {
-                    item.SellIn = item.SellIn - 1;
-                }
+            var specialItems = Items.Where(x => IsItemAgedBrie(x) || IsItemBackstagePass(x))
+                .Where(x => x.Quality < 50)
+                .ToList();
 
-                if (item.SellIn < 0)
-                {
-                    if (!IsItemAgedBrie(item))
-                    {
-                        if (!IsItemBackstagePass(item))
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (!IsItemSulfuras(item))
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
-                    }
-                }
-            }
+            specialItems.ForEach(IncreaseItemQuality);
+
+            var backstagePasses = specialItems.Where(IsItemBackstagePass);
+
+            var backstagePassesWithHighSellIn = backstagePasses
+                .Where(x => x.SellIn < 11 && x.Quality < 50)
+                .ToList();
+
+            backstagePassesWithHighSellIn.ForEach(IncreaseItemQuality);
+
+            var backstagePassesWithLowSellIn = backstagePasses
+                .Where(x => x.SellIn < 6 && x.Quality < 50)
+                .ToList();
+
+            backstagePassesWithLowSellIn.ForEach(IncreaseItemQuality);
+
+            Items.Where(x => !IsItemSulfuras(x))
+                .ToList()
+                .ForEach(DecreaseSellIn);
+
+            var itemsWithNegativeSellIn = Items.Where(x => x.SellIn < 0);
+
+            itemsWithNegativeSellIn.Where(IsItemConjured)
+                .Where(x => x.Quality > 0)
+                .ToList()
+                .ForEach(DecreaseItemQuality);
+
+            itemsWithNegativeSellIn.Where(x => !IsItemAgedBrie(x) && !IsItemBackstagePass(x) && !IsItemSulfuras(x) && x.Quality > 0)
+                .ToList()
+                .ForEach(DecreaseItemQuality);
+
+            itemsWithNegativeSellIn.Where(IsItemBackstagePass).ToList()
+                .ForEach(x => x.Quality = 0);
+            itemsWithNegativeSellIn.Where(IsItemAgedBrie)
+                .Where(x => x.Quality < 50)
+                .ToList()
+                .ForEach(IncreaseItemQuality);
         }
 
         private static bool IsItemSulfuras(Item item)
@@ -106,6 +82,26 @@ namespace csharp
         private static bool IsItemBackstagePass(Item item)
         {
             return item.Name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        private static bool IsItemConjured(Item item)
+        {
+            return item.Name == "Conjured Mana Cake";
+        }
+
+        private static void IncreaseItemQuality(Item item)
+        {
+            item.Quality++;
+        }
+
+        private static void DecreaseItemQuality(Item item)
+        {
+            item.Quality--;
+        }
+
+        private static void DecreaseSellIn(Item item)
+        {
+            item.SellIn--;
         }
     }
 }
